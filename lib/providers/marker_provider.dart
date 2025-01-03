404a8de34d1852e1deba5c6a82ca86ddbe558c9f
Dasun Mihiranga <da12345sun@gmail.com>
2025-01-03 17:21:13 +0530

    Add Google Maps with Riverpod state management for location and markers

---
+++ lib/providers/marker_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+final markerProvider = StateNotifierProvider<MarkerNotifier, Set<Marker>>((ref) {
+  return MarkerNotifier();
+});
+
+class MarkerNotifier extends StateNotifier<Set<Marker>> {
+  MarkerNotifier() : super({});
+
+  void addMarker(Marker marker) {
+    state = {marker};
+  }
+
+  void clearMarkers() {
+    state = {};
+  }
+}
