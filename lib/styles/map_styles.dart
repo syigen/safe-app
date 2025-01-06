6c867e9ebda9ca78b15e9d08910ed33a6677576d
Dasun Mihiranga <da12345sun@gmail.com>
2025-01-06 16:03:15 +0530

    add dark green theme and custom pinpoint marker into map

---
+++ lib/styles/map_styles.dart
@@ -0,0 +1,50 @@
+const String mapStyle = '''
+[
+  {
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#172525" }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#616161" }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      { "color": "#172525" }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#37474f" }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#9e9e9e" }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#172525" }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#3d3d3d" }
+    ]
+  }
+]
+''';
