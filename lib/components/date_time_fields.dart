16e07367127e537dd471e0873cd1bd50523421b7
Dasun Mihiranga <da12345sun@gmail.com>
2025-01-09 17:01:17 +0530

    remove form_field.dart file and separate codes , validate form

---
+++ lib/components/date_time_fields.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class DateTimeFields extends StatelessWidget {
+  final TextEditingController dateController;
+  final TextEditingController timeController;
+  final Color primaryColor;
+
+  DateTimeFields({
+    required this.dateController,
+    required this.timeController,
+    required this.primaryColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Date & time', style: TextStyle(color: Colors.white, fontSize: 16)),
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: dateController,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: primaryColor),
+                  ),
+                  suffixIcon: Icon(Icons.edit, color: primaryColor),
+                ),
+              ),
+            ),
+            SizedBox(width: 16),
+            Expanded(
+              child: TextField(
+                controller: timeController,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: primaryColor),
+                  ),
+                  suffixIcon: Icon(Icons.edit, color: primaryColor),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/bottom_panel.dart
+++ lib/pages/bottom_panel.dart
@@ -1,23 +1,23 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
+import '../components/casualties_section.dart';
+import '../components/date_time_fields.dart';
+import '../components/elephant_count_field.dart';
+import '../components/image_section.dart';
+import '../components/location_field.dart';
+import '../components/send_button.dart';
+import '../components/special_notes_section.dart';
 import '../styles/constants.dart';
 import '../model/alert_data.dart';
-import '../widgets/form_field.dart';
 import '../widgets/image_picker_button.dart';
 import '../widgets/time_button.dart';
-
 
 class BottomPanel extends StatefulWidget {
   final String selectedAddress;
   final LatLng selectedLocation;
-
-  final DateTime now = DateTime.now();
-  final String date = DateFormat('d MMMM').format(DateTime.now());
-  final String time = DateFormat('h:mm a').format(DateTime.now());
 
   BottomPanel(this.selectedAddress, this.selectedLocation);
 
@@ -30,10 +30,60 @@ class _BottomPanelState extends State<BottomPanel> {
   int _selectedElephantCount = 1;
   String _selectedCasualtyOption = 'No';
   String _specialNote = '';
-  String _selectedTimeButtonValue = 'Now'; // Initialize with default value
+  String _selectedTimeButtonValue = 'Now';
   File? _selectedImage;
   final List<AlertData> _alertDataList = [];
   final DraggableScrollableController _draggableController = DraggableScrollableController();
+
+  final TextEditingController _locationController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _timeController = TextEditingController();
+  final TextEditingController _elephantCountController = TextEditingController();
+  final TextEditingController _specialNotesController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _locationController.text = '${widget.selectedLocation.latitude}, ${widget.selectedLocation.longitude}';
+    _dateController.text = DateFormat('d MMMM').format(DateTime.now());
+    _timeController.text = DateFormat('h:mm a').format(DateTime.now());
+    _elephantCountController.text = _selectedElephantCount.toString();
+  }
+
+  @override
+  void dispose() {
+    _locationController.dispose();
+    _dateController.dispose();
+    _timeController.dispose();
+    _elephantCountController.dispose();
+    _specialNotesController.dispose();
+    _draggableController.dispose();
+    super.dispose();
+  }
+
+  bool _validateForm() {
+    if (_selectedElephantCount <= 0) {
+      _showError('Please select a valid number of elephants.');
+      return false;
+    }
+    if (_selectedCasualtyOption.isEmpty) {
+      _showError('Please specify if there are casualties.');
+      return false;
+    }
+    if (_specialNote.isEmpty) {
+      _specialNote = 'No special note provided.';
+    }
+    return true;
+  }
+
+  void _showError(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
 
   Future<void> _pickImage() async {
     final ImagePicker _picker = ImagePicker();
@@ -48,14 +98,14 @@ class _BottomPanelState extends State<BottomPanel> {
 
   void _storeAlertData() {
     final alertData = AlertData(
-      location: '${widget.selectedLocation.latitude}, ${widget.selectedLocation.longitude}',
-      date: widget.date,
-      time: widget.time,
-      elephantCount: _selectedElephantCount,
+      location: _locationController.text,
+      date: _dateController.text,
+      time: _timeController.text,
+      elephantCount: int.parse(_elephantCountController.text),
       casualtyOption: _selectedCasualtyOption,
-      specialNote: _specialNote,
+      specialNote: _specialNotesController.text,
       image: _selectedImage,
-      timeButtonValue: _selectedTimeButtonValue, // Pass the time button value
+      timeButtonValue: _selectedTimeButtonValue,
     );
 
     setState(() {
@@ -76,8 +126,8 @@ class _BottomPanelState extends State<BottomPanel> {
       },
       child: DraggableScrollableSheet(
         controller: _draggableController,
-        initialChildSize: 0.3,
-        minChildSize: 0.3,
+        initialChildSize: 0.35,
+        minChildSize: 0.35,
         maxChildSize: 0.95,
         builder: (context, scrollController) {
           return Container(
@@ -126,84 +176,79 @@ class _BottomPanelState extends State<BottomPanel> {
             'No. of elephants',
             style: TextStyle(color: Colors.white, fontSize: 16),
           ),
-          SizedBox(height: 12),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Center(
-              child: Row(
-                children: List.generate(5, (index) {
-                  final number = index == 4 ? '5+' : '${index + 1}';
-                  final isSelected = _selectedElephantCount == (index == 5 ? 5 : index + 1);
-                  return Padding(
-                    padding: EdgeInsets.all(5),
-                    child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _selectedElephantCount = index == 5 ? 5 : index + 1;
-                        });
-                      },
-                      child: Container(
-                        width: 40,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: isSelected ? primaryColor : Colors.transparent,
-                          border: Border.all(
-                            color: primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        child: Center(
-                          child: Text(
-                            number,
-                            style: TextStyle(
-                              color: isSelected ? Colors.black : primaryColor,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                }),
-              ),
-            ),
-          ),
           SizedBox(height: 20),
-          Row(
-            children: [
-              TimeButton(
-                text: 'Now',
-                isSelected: _selectedTimeButtonValue == 'Now',
-                onPressed: () {
-                  setState(() {
-                    _selectedTimeButtonValue = 'Now'; // Store the selected time button value
-                  });
-                },
-              ),
-              SizedBox(width: 8),
-              TimeButton(
-                text: '10 min.',
-                isSelected: _selectedTimeButtonValue == '10 min.',
-                onPressed: () {
-                  setState(() {
-                    _selectedTimeButtonValue = '10 min.'; // Store the selected time button value
-                  });
-                },
-              ),
-              SizedBox(width: 8),
-              TimeButton(
-                text: '30+ min.',
-                isSelected: _selectedTimeButtonValue == '30+ min.',
-                onPressed: () {
-                  setState(() {
-                    _selectedTimeButtonValue = '30+ min.'; // Store the selected time button value
-                  });
-                },
-              ),
-            ],
-          ),
+          _buildElephantCountSelector(),
+          SizedBox(height: 30),
+          _buildTimeButtons(),
         ],
       ),
+    );
+  }
+
+  Widget _buildElephantCountSelector() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(20, (index) {
+          final number = index == 19 ? '20+' : '${index + 1}';
+          final isSelected = _selectedElephantCount == (index == 19 ? 20 : index + 1);
+          return Padding(
+            padding: EdgeInsets.all(10),
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _selectedElephantCount = index == 19 ? 20 : index + 1;
+                  _elephantCountController.text = _selectedElephantCount.toString();
+                });
+              },
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: isSelected ? primaryColor : Colors.transparent,
+                  border: Border.all(
+                    color: primaryColor,
+                    width: 2,
+                  ),
+                ),
+                child: Center(
+                  child: Text(
+                    number,
+                    style: TextStyle(
+                      color: isSelected ? Colors.black : primaryColor,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
+  Widget _buildTimeButtons() {
+    return Row(
+      children: [
+        TimeButton(
+          text: 'Now',
+          isSelected: _selectedTimeButtonValue == 'Now',
+          onPressed: () => setState(() => _selectedTimeButtonValue = 'Now'),
+        ),
+        SizedBox(width: 30),
+        TimeButton(
+          text: '10 min.',
+          isSelected: _selectedTimeButtonValue == '10 min.',
+          onPressed: () => setState(() => _selectedTimeButtonValue = '10 min.'),
+        ),
+        SizedBox(width: 30),
+        TimeButton(
+          text: '30+ min.',
+          isSelected: _selectedTimeButtonValue == '30+ min.',
+          onPressed: () => setState(() => _selectedTimeButtonValue = '30+ min.'),
+        ),
+      ],
     );
   }
 
@@ -213,116 +258,93 @@ class _BottomPanelState extends State<BottomPanel> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.arrow_back, color: Colors.white),
-                onPressed: () {
-                  _draggableController.animateTo(
-                    0.3,
-                    duration: Duration(milliseconds: 300),
-                    curve: Curves.easeOut,
-                  );
-                  setState(() {
-                    _isExpanded = false;
-                  });
-                },
-              ),
-              const Text(
-                'Send alert',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
+          _buildHeader(),
+          SizedBox(height: 24),
+          LocationField(
+            locationController: _locationController,
+            primaryColor: primaryColor, // Pass primaryColor to LocationField
+          ),
+          SizedBox(height: 16),
+          DateTimeFields(
+            dateController: _dateController,
+            timeController: _timeController,
+            primaryColor: primaryColor,
+          ),
+          SizedBox(height: 16),
+          ElephantCountField(
+            elephantCountController: _elephantCountController,
+            primaryColor: primaryColor,
           ),
           SizedBox(height: 24),
-          FormFieldWidget(label: 'Location', value: '${widget.selectedLocation.latitude}, ${widget.selectedLocation.longitude}'),
-          FormFieldWidget(label: 'Date & time', value: widget.date, secondValue: widget.time),
-          FormFieldWidget(label: 'No. of elephants', value: '$_selectedElephantCount'),
-          SizedBox(height: 24),
-          const Text('Casualties', style: TextStyle(color: Colors.white, fontSize: 16)),
-          Row(
-            children: [
-              Radio<String>(
-                value: 'Yes',
-                groupValue: _selectedCasualtyOption,
-                onChanged: (value) {
-                  setState(() {
-                    _selectedCasualtyOption = value!;
-                  });
-                },
-                activeColor: primaryColor,
-              ),
-              Text('Yes', style: TextStyle(color: Colors.white)),
-              SizedBox(width: 32),
-              Radio<String>(
-                value: 'No',
-                groupValue: _selectedCasualtyOption,
-                onChanged: (value) {
-                  setState(() {
-                    _selectedCasualtyOption = value!;
-                  });
-                },
-                activeColor: primaryColor,
-              ),
-              Text('No', style: TextStyle(color: Colors.white)),
-            ],
-          ),
-          SizedBox(height: 24),
-          const Text('Special notes (Optional)', style: TextStyle(color: Colors.white, fontSize: 16)),
-          SizedBox(height: 8),
-          TextField(
-            onChanged: (value) {
-              _specialNote = value;
+          CasualtiesSection(
+            selectedCasualtyOption: _selectedCasualtyOption,
+            onCasualtyOptionChanged: (value) {
+              setState(() {
+                _selectedCasualtyOption = value!;
+              });
             },
-            decoration: InputDecoration(
-              hintText: 'Type here...',
-              hintStyle: TextStyle(color: Colors.grey),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: primaryColor),
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-            style: TextStyle(color: Colors.white),
-            maxLines: 3,
+            primaryColor: primaryColor,
           ),
           SizedBox(height: 24),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ImagePickerButton(onPressed: _pickImage),
-              SizedBox(height: 16),
-              if (_selectedImage != null)
-                Column(
-                  children: [
-                    Image.file(_selectedImage!, height: 50, width: double.infinity, fit: BoxFit.cover),
-                    SizedBox(height: 8),
-                  ],
-                ),
-            ],
+          SpecialNotesSection(
+            specialNotesController: _specialNotesController,
+            onChanged: (value) => setState(() => _specialNote = value),
           ),
           SizedBox(height: 24),
-          ElevatedButton(
+          ImageSection(
+            onPressed: _pickImage,
+            selectedImage: _selectedImage,
+          ),
+          SizedBox(height: 24),
+          SendButton(
             onPressed: () {
-              _storeAlertData();
+              print('Elephant Count before validation: $_selectedElephantCount'); // Debug print
+              if (_validateForm()) {
+                _storeAlertData();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Alert data stored successfully!'),
+                    backgroundColor: Colors.green,
+                  ),
+                );
+              } else {
+                print('Validation failed'); // Debug print to show validation failure
+              }
             },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: primaryColor,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-              minimumSize: Size(double.infinity, 48),
-            ),
-            child: Text('Send Alert', style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold)),
           ),
           SizedBox(height: 24),
         ],
       ),
     );
   }
+
+
+  Widget _buildHeader() {
+    return Row(
+      children: [
+        IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            _draggableController.animateTo(
+              0.3,
+              duration: Duration(milliseconds: 300),
+              curve: Curves.easeOut,
+            );
+            setState(() {
+              _isExpanded = false;
+            });
+          },
+        ),
+        Text(
+          'Send alert',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+
 }
