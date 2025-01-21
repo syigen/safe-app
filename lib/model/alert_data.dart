ae1544c79ee3f3560e627ecf13b7380bec486851
hasin.pramodya <pramodyahasin@gmail.com>
2025-01-21 15:55:55 +0530

    create alert model class

---
+++ lib/model/alert_data.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+class AlertData {
+  final String location;
+  final String date;
+  final String time;
+  final int elephantCount;
+  final String casualtyOption;
+  final String specialNote;
+  final File? image;
+  final String timeButtonValue;
+
+  AlertData({
+    required this.location,
+    required this.date,
+    required this.time,
+    required this.elephantCount,
+    required this.casualtyOption,
+    required this.specialNote,
+    this.image,
+    required this.timeButtonValue,
+  });
+
+  @override
+  String toString() {
+    return 'Location: $location\n'
+        'Date: $date\n'
+        'Time: $time\n'
+        'Elephant Count: $elephantCount\n'
+        'Casualty: $casualtyOption\n'
+        'Special Notes: $specialNote\n'
+        'Image: ${image != null ? 'Image Selected at ${image!.path}' : 'No Image'}\n'
+        'Time Button Value: $timeButtonValue';
+  }
+}
