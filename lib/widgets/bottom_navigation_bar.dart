6e6efacf3997a640f533c889a1c4070d9d17df51
AminduUK <aminduumayangakule23@gmail.com>
2025-01-30 11:38:43 +0530

    create custom bottom_nav_bar

---
+++ lib/widgets/bottom_navigation_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const CustomBottomNavBar({
+    Key? key,
+    this.currentIndex = 0,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: const Color(0xFF032221),
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: const Color(0xFF00FF9D),
+      unselectedItemColor: Colors.grey,
+      currentIndex: currentIndex,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.explore), label: 'Map'),
+        BottomNavigationBarItem(icon: Icon(Icons.grid_view), label: 'Services'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.notifications),
+          label: 'Notifications',
+        ),
+      ],
+    );
+  }
+}
