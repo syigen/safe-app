038775530379918c24fcc9c5fcf2605fb143c756
hasin.pramodya <pramodyahasin@gmail.com>
2025-01-30 14:38:15 +0530

    update the model class

--- lib/model/alert_data.dart
+++ lib/model/alert_data.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 
-enum DistanceRange { m100, m200, m500, m1000 }
+enum DistanceRange { m100, m200, m500, m1000, near }
 
 class AlertData {
   final String location;
   final String date;
+  final String time;
   final int elephantCount;
   final String casualtyOption;
   final String specialNote;
@@ -15,12 +16,14 @@ class AlertData {
   AlertData({
     required this.location,
     required this.date,
+    required this.time,
     required this.elephantCount,
     required this.casualtyOption,
     required this.specialNote,
     this.image,
     required this.timeButtonValue,
     required this.distanceRange,
+    required imageUrl,
 
   });
 
