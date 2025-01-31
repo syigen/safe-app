bf13ed5eabac567c83cf4d3bf5605f4e94e56042
AminduUK <aminduumayangakule23@gmail.com>
2025-01-31 13:17:45 +0530

    create new service_page.dart

---
+++ lib/pages/service_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ServicePage extends StatelessWidget {
+  const ServicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Service'),
+      ),
+      body: const Center(
+        child: Text('Service Page'),
+      ),
+    );
+  }
+}
+
