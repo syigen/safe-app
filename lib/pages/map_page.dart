2559e0152b451aab32d20bb66d02daab10a4224f
nathija-nimantha <nathija.max99@gmail.com>
2025-02-07 15:20:04 +0530

    update back button navigation on map_page.dart

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,18 +1,38 @@
-import 'package:flutter/material.dart';
+/*
+ * Copyright 2024-Present, Syigen Ltd. and Syigen Private Limited. All rights reserved.
+ *
+ */
 
-class MapPage extends StatelessWidget {
-  const MapPage({super.key});
+import 'package:flutter/material.dart';
+import 'package:safe_app/pages/main_page.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
 
   @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Map'),
-      ),
-      body: const Center(
-        child: Text('Map Page'),
+    return WillPopScope(
+      onWillPop: () async {
+        final mainPageState = context.findAncestorStateOfType<MainPageState>();
+        if (mainPageState != null) {
+          mainPageState.navigateToHome();
+        }
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Map Page'),
+          automaticallyImplyLeading: false,
+        ),
+        body: const Center(
+          child: Text('Map Page'),
+        ),
       ),
     );
   }
 }
-
