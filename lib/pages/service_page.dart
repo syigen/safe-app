b7691953efe09e8c8f974b5527301ece0d8434c4
nathija-nimantha <nathija.max99@gmail.com>
2025-02-07 15:19:46 +0530

    update back button navigation on service_page.dart

--- lib/pages/service_page.dart
+++ lib/pages/service_page.dart
@@ -1,18 +1,33 @@
+/*
+ * Copyright 2024-Present, Syigen Ltd. and Syigen Private Limited. All rights reserved.
+ *
+ */
+
 import 'package:flutter/material.dart';
+import 'package:safe_app/pages/main_page.dart';
 
 class ServicePage extends StatelessWidget {
   const ServicePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Service'),
-      ),
-      body: const Center(
-        child: Text('Service Page'),
+    return WillPopScope(
+      onWillPop: () async {
+        final mainPageState = context.findAncestorStateOfType<MainPageState>();
+        if (mainPageState != null) {
+          mainPageState.navigateToHome();
+        }
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Service'),
+          automaticallyImplyLeading: false,
+        ),
+        body: const Center(
+          child: Text('Service Page'),
+        ),
       ),
     );
   }
 }
-
