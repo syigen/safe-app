b559f259d80d46ff463a1906932952cc21f665fe
nathija-nimantha <nathija.max99@gmail.com>
2025-02-07 15:19:56 +0530

    update back button navigation on main_page.dart

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2024-Present, Syigen Ltd. and Syigen Private Limited. All rights reserved.
+ *
+ */
+
 import 'package:flutter/material.dart';
 import 'package:safe_app/auth/auth_service.dart';
 import 'package:safe_app/pages/home_page.dart';
@@ -10,15 +15,15 @@ class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
-  _MainPageState createState() => _MainPageState();
+  MainPageState createState() => MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class MainPageState extends State<MainPage> {
   int _currentIndex = 0;
 
   final List<Widget> _pages = [
     HomeScreen(authClient: SupabaseAuthClient()),
-    const MapPage(),
+    const MapScreen(),
     const ServicePage(),
     const NotificationsPage(),
   ];
@@ -26,6 +31,12 @@ class _MainPageState extends State<MainPage> {
   void _onItemTapped(int index) {
     setState(() {
       _currentIndex = index;
+    });
+  }
+
+  void navigateToHome() {
+    setState(() {
+      _currentIndex = 0;
     });
   }
 
