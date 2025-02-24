685b1abe1ce6ceb34bc9059ff61b40e0e3d752fb
nathija-nimantha <nathija.max99@gmail.com>
2025-02-24 15:36:05 +0530

    update service_page.dart

--- lib/pages/service_page.dart
+++ lib/pages/service_page.dart
@@ -4,28 +4,58 @@
  */
 
 import 'package:flutter/material.dart';
-import 'package:safe_app/pages/main_page.dart';
+import '../widgets/service_card.dart';
 
 class ServicePage extends StatelessWidget {
   const ServicePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        final mainPageState = context.findAncestorStateOfType<MainPageState>();
-        if (mainPageState != null) {
-          mainPageState.navigateToHome();
-        }
-        return false;
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Service'),
-          automaticallyImplyLeading: false,
-        ),
-        body: const Center(
-          child: Text('Service Page'),
+    return Scaffold(
+      backgroundColor: const Color(0xFF032221),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                'All Services and tools',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return GridView(
+                    padding: const EdgeInsets.all(16.0),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: constraints.maxWidth > 600 ? 4 : 2,
+                      childAspectRatio: 163 / 80,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                    ),
+                    children: [
+                      ServiceCard(
+                        title: 'Contacts',
+                        icon: 'assets/icons/contact.png',
+                        onTap: () {},
+                      ),
+                      ServiceCard(
+                        title: 'Safety tips',
+                        icon: 'assets/icons/safety.png',
+                        onTap: () {},
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
