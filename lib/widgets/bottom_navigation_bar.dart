6bf34c7489078bf1f4548976f16d09becfcdf6df
hasin.pramodya <pramodyahasin@gmail.com>
2025-03-06 12:39:43 +0530

    fix minor bugs in bottom_navigation_bar.dart

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -15,15 +15,27 @@ class CustomBottomNavigationBar extends StatelessWidget {
     return BottomNavigationBar(
       backgroundColor: const Color(0xFF032221),
       type: BottomNavigationBarType.fixed,
-      selectedItemColor: const Color(0xFF00FF9D),
-      unselectedItemColor: Colors.grey,
+      selectedItemColor: const Color(0xFF00FF81),
+      unselectedItemColor: const Color(0xFFF1F7F6),
       currentIndex: currentIndex,
       onTap: onTap,
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.explore_outlined), label: 'Map'),
-        BottomNavigationBarItem(icon: Icon(Icons.apps_outlined), label: 'Services'),
-        BottomNavigationBarItem(icon: Icon(Icons.notifications_outlined), label: 'Notifications'),
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(currentIndex == 0 ? Icons.home : Icons.home_outlined),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(currentIndex == 1 ? Icons.explore_outlined : Icons.explore_outlined),
+          label: 'Map',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(currentIndex == 2 ? Icons.apps_outlined : Icons.apps_outlined),
+          label: 'Services',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(currentIndex == 3 ? Icons.notifications : Icons.notifications_outlined),
+          label: 'Notifications',
+        ),
       ],
     );
   }
