a0a313bbab68376e167ddabbff73506a25dbeafd
AminduUK <aminduumayangakule23@gmail.com>
2025-03-21 13:19:19 +0530

    create the model class for news

---
+++ lib/model/comment_data.dart
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024-Present, Syigen Ltd. and Syigen Private Limited. All rights reserved.
+ *
+ */
+
+class Comment {
+  final int id;
+  final int newsId;
+  final String userId;
+  final String content;
+  final DateTime createdAt;
+  final Map<String, dynamic>? userProfile;
+
+  Comment({
+    required this.id,
+    required this.newsId,
+    required this.userId,
+    required this.content,
+    required this.createdAt,
+    this.userProfile,
+  });
+
+  factory Comment.fromJson(Map<String, dynamic> json) {
+    return Comment(
+      id: json['id'],
+      newsId: json['news_id'],
+      userId: json['user_id'],
+      content: json['content'],
+      createdAt: DateTime.parse(json['created_at']),
+      userProfile: json['profiles'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'news_id': newsId,
+      'user_id': userId,
+      'content': content,
+      'created_at': createdAt.toIso8601String(),
+      'profiles': userProfile,
+    };
+  }
+}
