fccbde5dd9f3042e1332a280a46ccd21c65397ae
AminduUK <aminduumayangakule23@gmail.com>
2025-04-08 10:17:32 +0530

    change the copyright and code optimization in models

--- lib/model/user_data.dart
+++ lib/model/user_data.dart
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2024-Present, Syigen Ltd. and Syigen Private Limited. All rights reserved.
+ * Licensed under the GNU GENERAL PUBLIC LICENSE
+ *                      Version 3  (See LICENSE.md orhttps://www.gnu.org/licenses/gpl-3.0.en.html).
+ */
+
 class UserData {
   final String fullName;
   final String avatarUrl;
